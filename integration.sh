0e46e9af7dbd700b9a265fda36e143320ecbaf25
Aquiles Barreto <48977088+barretoaquiles@users.noreply.github.com>
2022-02-05 13:19:25 -0300

    Add integration.sh
    The bash file integration.sh run continuous integration & delivery (CI/CD) automatically.

---
+++ integration.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git add .
+git commit -m "New Commit in Integration on `date +'%Y-%m-%d %H:%M:%S'`"
+git push https://aquilesnubi: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@github.com/aquilesnubi/cml.git integration
